3601a2ed0ae9df25a0e9465d4f98b03d32122321
Zedrick Wills Geroy Ara <zedrick.ara@gmail.com>
2017-09-20 10:11:39 -1000

    buildHtmlElements function works!

--- script.js
+++ script.js
@@ -7,11 +7,22 @@
 // });
 
 
-function buildHtmlElements(responseData){
+function buildHtmlElements(){
 // Dynamically build HTML elements WITH data that was GET from JSON
-// In class films, add classes
-$("#films").append(new Array(++4).join("<div></div>")
-});
+// Build the elements
+let title = $("<h2 class='film-title'></h2>").text("FILM TITLE");
+let episode = $("<p class='film-episode'></p>").text("FILM EPISODE NUMBER");
+let director = $("<p class='film-director'></p>").text("FILM DIRECTOR NAME");
+let releasedate = $("<p class='film-release-date'></p>").text("FILM RELEASE DATE");
+
+// Append the elements
+$("body").append(title, episode, director, releasedate);
+}
+
+function getFilmData(id){
+
+}
+
 
   $(document).ready(function(){
     $("button").click(function(){
