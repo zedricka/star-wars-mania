849f22be28daaf3f152c92f0617ef7b512da1039
Zedrick Wills Geroy Ara <zedrick.ara@gmail.com>
2017-09-20 10:55:32 -1000

    Working on AJAX call

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// $(document).ready(function(){
-//     $("button").click(function(){
-//         $.ajax({url: "http swapi.co/api/planets/1/", success: function(result){
-//             $("#films").html(result);
-//         }});
-//     });
-// });
+// console.log("Sanity Check");
 
-
-function buildHtmlElements(){
-// Dynamically build HTML elements WITH data that was GET from JSON
+function buildHtmlElements(){ // creates HTML elements dynamically
 // Build the elements
 let title = $("<h2 class='film-title'></h2>").text("FILM TITLE");
 let episode = $("<p class='film-episode'></p>").text("FILM EPISODE NUMBER");
@@ -17,17 +9,37 @@ let releasedate = $("<p class='film-release-date'></p>").text("FILM RELEASE DATE
 
 // Append the elements
 $("body").append(title, episode, director, releasedate);
+} 
+
+function getFilmData(id){ // GETS the film data
+
 }
 
-function getFilmData(id){
-
-}
-
-
-  $(document).ready(function(){
-    $("button").click(function(){
-      $.ajax({url: "http swapi.co/api/planets/1/", success: function(result){
+// AJAX to get the data from SWAPI
+/*  $("button").click(function(){
+      $.ajax({url: "http https://swapi.co/api/films/", success: function(data, status){
           $("#films").html(result);
       }});
     });
+*/
+
+
+$("button").click(function(){
+  $.ajax({url: "https://swapi.co/api/films/",
+    type: "GET",
+    success:function(result){
+      console.log(result);
+    $(".films").html(result);
+    }
   });
+    fail:function(error){
+      alert("Something is wrong, very wrong.");
+    };
+  //   success:function(result){
+  //     console.log(result);
+  //   $(".films").html(result);
+  // }}),
+  //   fail:function(error){
+  //     alert("Something is wrong, very wrong.");
+  // }
+// });
