44a59faa6c98dd046e1b857206273ec992e89a7f
Zedrick Wills Geroy Ara <zedrick.ara@gmail.com>
2017-09-22 09:30:01 -1000

    Data Calling Works!

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // console.log("Sanity Check");
 
-function buildHtmlElements(){ // creates HTML elements dynamically
+function buildHtmlElements(responseData){ // creates HTML elements dynamically
 // Build the elements
 let title = $("<h2 class='film-title'></h2>").text("FILM TITLE");
 let episode = $("<p class='film-episode'></p>").text("FILM EPISODE NUMBER");
@@ -11,35 +11,25 @@ let releasedate = $("<p class='film-release-date'></p>").text("FILM RELEASE DATE
 $("body").append(title, episode, director, releasedate);
 } 
 
+
 function getFilmData(id){ // GETS the film data
 
 }
 
-// AJAX to get the data from SWAPI
-/*  $("button").click(function(){
-      $.ajax({url: "http https://swapi.co/api/films/", success: function(data, status){
-          $("#films").html(result);
-      }});
-    });
-*/
 
-
+// This calls the data
 $("button").click(function(){
-  $.ajax({url: "https://swapi.co/api/films/",
+  $.ajax({
+    url: "https://swapi.co/api/films/",
     type: "GET",
-    success:function(result){
-      console.log(result);
-    $(".films").html(result);
-    }
-  });
+    
+    success:function(data){
+      console.log(data);
+      $(".films").html(data);
+    },
+    
     fail:function(error){
       alert("Something is wrong, very wrong.");
-    };
-  //   success:function(result){
-  //     console.log(result);
-  //   $(".films").html(result);
-  // }}),
-  //   fail:function(error){
-  //     alert("Something is wrong, very wrong.");
-  // }
-// });
+    },
+  });
+});
